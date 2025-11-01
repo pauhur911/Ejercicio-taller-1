58a8c658708a7268da43768198b2704b0f0dc5d7
Paula H <pauhur911@gmail.com>
2025-10-31 20:25:14 -0500

    cambios

--- PH.js
+++ PH.js
@@ -1,7 +1,7 @@
 function saludar (){
-    console.log("cambios en main y el mismo archivo");
+    console.log("cambios en main, cambios, cambios");
 }
 
 function saludar (){
-    console.log("hola, ingrese su numero de documento");
+    console.log("hola, ingrese su numero");
 }
